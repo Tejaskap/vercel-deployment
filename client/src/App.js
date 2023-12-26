28fc593bc701de680b5e735f65cc66b63a995e33
Tejas <tejaskapoor97x@gmail.com>
2023-12-26 17:36:29 +0100

    server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [serverMessage, setServerMessage] = useState("");
+
+  useEffect(() => {
+    // Fetch data from the server when the component mounts
+    fetchServerMessage();
+  }, []);
+
+  const fetchServerMessage = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/"); // Update the URL to match your server's address
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.text();
+      console.log("Server message:", data);
+      setServerMessage(data);
+    } catch (error) {
+      console.error("Error fetching data from the server:", error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {serverMessage || "Loading..."}
+          <br />
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Server is coming..");
+// Enable CORS for all routes
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World from the Server!");
 });
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`);
+});

baf7fe0206d0b5e050f471fa906ed41a0a09f260
Tejas <tejaskapoor97x@gmail.com>
2023-12-27 02:11:00 +0100

    uploading proper app

--- client/app/page.js
+++ client/app/page.js
@@ -10,7 +10,7 @@ import TimeSlots from "./components/TimeSlots";
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://vercel-deployment-client-eosin.vercel.app";
+    : "https://vercel-deployment-client-eosin.vercel.app/";
 
 const API_ENDPOINT = `${API_BASE_URL}/api/display-events`;
 const API_ENDPOINT_CREATE_EVENT = `${API_BASE_URL}/api/create-event`;

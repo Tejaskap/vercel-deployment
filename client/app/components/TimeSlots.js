dbacb911fcc673fe6183b337ddee240ca23edac7
Tejas <tejaskapoor97x@gmail.com>
2023-12-28 03:05:05 +0100

    list added button green

--- client/app/components/TimeSlots.js
+++ client/app/components/TimeSlots.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useMemo, useState, useRef } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import LoadingIndicator from "./LoadingIndicator";
+
 // Utility function to get day name
 const getDayName = (dayIndex) => {
   const days = [
@@ -16,54 +17,76 @@ const getDayName = (dayIndex) => {
 
 const TimeSlots = ({ timeSlots, data, selectedDate, onAddNow }) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [addedSlots, setAddedSlots] = useState(new Set());
+  const [addedSlots, setAddedSlots] = useState([]);
 
   const isTimeOverlap = (start1, end1, start2, end2) => {
     return start1 < end2 && end1 > start2;
   };
 
   const availableTimeSlots = useMemo(() => {
+    if (!selectedDate) {
+      return [];
+    }
+
     return timeSlots.filter((slot) => {
-      return !data.some((event) =>
-        isTimeOverlap(
-          new Date(event.start.dateTime),
-          new Date(event.end.dateTime),
-          new Date(
-            selectedDate.getFullYear(),
-            selectedDate.getMonth(),
-            selectedDate.getDate(),
-            parseInt(slot.startTime.split(":")[0], 10),
-            0,
-            0
-          ),
-          new Date(
-            selectedDate.getFullYear(),
-            selectedDate.getMonth(),
-            selectedDate.getDate(),
-            parseInt(slot.endTime.split(":")[0], 10),
-            0,
-            0
-          )
-        )
+      const slotStartTime = new Date(
+        selectedDate.getFullYear(),
+        selectedDate.getMonth(),
+        selectedDate.getDate(),
+        parseInt(slot.startTime.split(":")[0], 10),
+        0,
+        0
       );
+
+      const slotEndTime = new Date(
+        selectedDate.getFullYear(),
+        selectedDate.getMonth(),
+        selectedDate.getDate(),
+        parseInt(slot.endTime.split(":")[0], 10),
+        0,
+        0
+      );
+
+      return !data.some((event) => {
+        const eventStartTime = new Date(event.start.dateTime);
+        const eventEndTime = new Date(event.end.dateTime);
+
+        return isTimeOverlap(
+          eventStartTime,
+          eventEndTime,
+          slotStartTime,
+          slotEndTime
+        );
+      });
     });
   }, [data, selectedDate, timeSlots]);
 
   useEffect(() => {
-    setIsLoading(true); // Show loader when calendar changes
+    setIsLoading(true);
 
     // Simulate an asynchronous operation (e.g., fetching data)
     const delay = setTimeout(() => {
-      setIsLoading(false); // Hide loader after data is fetched (replace this with your actual data fetching logic)
+      setIsLoading(false);
     }, 1000);
 
-    return () => clearTimeout(delay); // Cleanup function to clear the timeout on component unmount
+    return () => clearTimeout(delay);
   }, [selectedDate, timeSlots]);
 
   const handleAddNow = (slot) => {
     const timeSlotString = `${slot.startTime} - ${slot.endTime}`;
-    if (!addedSlots.has(timeSlotString)) {
-      setAddedSlots(new Set([...addedSlots, timeSlotString]));
+    const slotInfo = {
+      timeSlotString,
+      date: selectedDate.toDateString(),
+    };
+
+    if (
+      !addedSlots.some(
+        (addedSlot) =>
+          addedSlot.timeSlotString === timeSlotString &&
+          addedSlot.date === slotInfo.date
+      )
+    ) {
+      setAddedSlots([...addedSlots, slotInfo]);
       onAddNow({
         startTime: slot.startTime,
         endTime: slot.endTime,
@@ -91,16 +114,29 @@ const TimeSlots = ({ timeSlots, data, selectedDate, onAddNow }) => {
                 <div className="flex space-x-4">
                   <button
                     className={`${
-                      addedSlots.has(`${slot.startTime} - ${slot.endTime}`)
+                      addedSlots.some(
+                        (addedSlot) =>
+                          addedSlot.timeSlotString ===
+                            `${slot.startTime} - ${slot.endTime}` &&
+                          addedSlot.date === selectedDate.toDateString()
+                      )
                         ? "bg-green-500"
                         : "bg-blue-500"
                     } text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800 text-sm transition duration-300 ease-in-out`}
                     onClick={() => handleAddNow(slot)}
-                    disabled={addedSlots.has(
-                      `${slot.startTime} - ${slot.endTime}`
+                    disabled={addedSlots.some(
+                      (addedSlot) =>
+                        addedSlot.timeSlotString ===
+                          `${slot.startTime} - ${slot.endTime}` &&
+                        addedSlot.date === selectedDate.toDateString()
                     )}
                   >
-                    {addedSlots.has(`${slot.startTime} - ${slot.endTime}`)
+                    {addedSlots.some(
+                      (addedSlot) =>
+                        addedSlot.timeSlotString ===
+                          `${slot.startTime} - ${slot.endTime}` &&
+                        addedSlot.date === selectedDate.toDateString()
+                    )
                       ? "Added"
                       : "Add Now"}
                   </button>
